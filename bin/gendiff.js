f3b6b13df3216bc79607f97470bc57241a684a3c
Albert Shubin <albert.shubin@gmail.com>
2023-08-01 00:09:00 +0400

    feat: add arguments <filepath1>, <filepath2>

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,7 +4,10 @@ import { Command } from 'commander/esm.mjs';
 const program = new Command();
 
 program
+  .argument('<filepath1>', 'path to first file')
+  .argument('<filepath2>', 'path to second file')
   .description('Compares two configuration files and shows a difference.')
-  .option('-V, --version', 'output the version number');
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
