8176d37114eacfc97f8c178ca7f5a082bc2a6ac9
Albert Shubin <albert.shubin@gmail.com>
2023-08-17 00:02:27 +0400

    test: add test genDiff

---
+++ __tests__/index.test.js
@@ -0,0 +1,17 @@
+import  genDiff  from '../src/index.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { readFileSync } from 'node:fs';
+import { join } from 'node:path';
+import { expect } from '@jest/globals';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
+const expectFile = readFileSync(getFixturePath('expectFile.txt'), 'utf-8');
+
+test('test genDiff', () => {
+    const file1 = getFixturePath('file1.json');
+    const file2 = getFixturePath('file2.json');
+    expect(genDiff(file1, file2)).toBe(expectFile);
+  });
