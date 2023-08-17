dfb479e6b7bc5a83eb6cc47e410ffb54587551c2
Albert Shubin <albert.shubin@gmail.com>
2023-08-17 19:47:02 +0400

    test: add test for yaml/yml; unsupported extension

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,8 +10,35 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
 const expectFile = readFileSync(getFixturePath('expectFile.txt'), 'utf-8');
 
-test('test genDiff', () => {
-  const file1 = getFixturePath('file1.json');
-  const file2 = getFixturePath('file2.json');
+test('test genDiff json', () => {
+  const file1 = getFixturePath('json-test-file-1.json');
+  const file2 = getFixturePath('json-test-file-2.json');
   expect(genDiff(file1, file2)).toBe(expectFile);
+});
+
+test('test genDiff yaml', () => {
+  const file1 = getFixturePath('yaml-test-file-1.yaml');
+  const file2 = getFixturePath('yaml-test-file-2.yaml');
+  expect(genDiff(file1, file2)).toBe(expectFile);
+});
+
+test('test genDiff yml', () => {
+  const file1 = getFixturePath('yaml-test-file-1.yml');
+  const file2 = getFixturePath('yaml-test-file-2.yml');
+  expect(genDiff(file1, file2)).toBe(expectFile);
+});
+
+test('test genDiff without extension', () => {
+  const file1 = getFixturePath('json-test-file-1');
+  const file2 = getFixturePath('json-test-file-2');
+  expect(genDiff(file1, file2)).toBe(expectFile);
+});
+
+test('test genDiff unsupported extension', () => {
+  const file1 = getFixturePath('yaml-test-file-1.yaml');
+  const file2 = getFixturePath('unsupported.woof');
+  function genDiffError() {
+    genDiff(file1, file2);
+  }
+  expect(genDiffError).toThrow(new Error('File extension error.'));
 });
