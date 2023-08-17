193779386ddd5dcab904bd6b01a194628419a67f
Albert Shubin <albert.shubin@gmail.com>
2023-08-17 19:45:42 +0400

    feat: add js-yaml parser

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
-import { resolve } from 'node:path';
-import { cwd } from 'node:process';
+import { resolve, extname } from 'node:path';
 import { readFileSync } from 'node:fs';
-import buildTree from './buildTree.js';
+import { cwd } from 'node:process';
 import parsedFile from './parser.js';
+import buildTree from './buildTree.js';
 
 const buildAbsolutePath = (filepath) => {
   const absolutFilePath = resolve(cwd(), filepath);
   return absolutFilePath;
 };
 
+const getExtension = (filepath) => {
+  const fileExtension = extname(filepath);
+  return fileExtension;
+};
+
 const readFile = (file) => readFileSync(file, 'utf8');
 
 export default (filepath1, filepath2) => {
-  const file1 = parsedFile(readFile(buildAbsolutePath(filepath1)));
-  const file2 = parsedFile(readFile(buildAbsolutePath(filepath2)));
+  const file1 = parsedFile(readFile(buildAbsolutePath(filepath1)), getExtension(filepath1));
+  const file2 = parsedFile(readFile(buildAbsolutePath(filepath2)), getExtension(filepath2));
 
   const result = buildTree(file1, file2);
 
--- src/parser.js
+++ src/parser.js
@@ -1,1 +1,14 @@
-export default (file) => JSON.parse(file);
+import yaml from 'js-yaml';
+
+export default (file, extension) => {
+  switch (extension) {
+    case '':
+    case '.json':
+      return JSON.parse(file);
+    case '.yml':
+    case '.yaml':
+      return yaml.load(file);
+    default:
+      throw new Error('File extension error.');
+  }
+};
