a52e8477f767014dbcf40ca0967d4c0a37d18b26
Albert Shubin <albert.shubin@gmail.com>
2023-08-20 22:21:13 +0400

    refactor: fix padding

--- src/index.js
+++ src/index.js
@@ -5,19 +5,18 @@ import parsedFile from './parser.js';
 import buildTree from './buildTree.js';
 import makeBeautiful from './formatters/index.js';
 
-const buildAbsolutePath = (filepath) => {
-  const absolutFilePath = resolve(cwd(), filepath);
-  return absolutFilePath;
-};
-
-const getExtension = (filepath) => {
-  const fileExtension = extname(filepath);
-  return fileExtension;
-};
-
-const readFile = (file) => readFileSync(file, 'utf8');
-
 export default (filepath1, filepath2, format = 'stylish') => {
+  const buildAbsolutePath = (filepath) => {
+    const absolutFilePath = resolve(cwd(), filepath);
+    return absolutFilePath;
+  };
+
+  const getExtension = (filepath) => {
+    const fileExtension = extname(filepath);
+    return fileExtension;
+  };
+
+  const readFile = (file) => readFileSync(file, 'utf8');
   const file1 = parsedFile(readFile(buildAbsolutePath(filepath1)), getExtension(filepath1));
   const file2 = parsedFile(readFile(buildAbsolutePath(filepath2)), getExtension(filepath2));
   const treeDiff = buildTree(file1, file2);
